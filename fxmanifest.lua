1d07211c9538585bf72cfd031b81c1cd5ddf122a
SafeHavenRoleplay <therealglitchxy@gmail.com>
2022-11-29 11:39:28 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Kara Webber'
+description 'Extras Script for Safe Haven Roleplay'
+version '1.0.0'
+
+client_scripts{
+	'extras-so.lua',
+	--'extras-pd.lua',
+	'extras-st.lua',
+	}
